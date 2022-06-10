4a4088d8fc0ec7281375ed229e13aa0e5f7753c4
surjith1 <rvdsurjith@gmail.com>
2022-06-10 10:00:20 +0530

    login token added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { MongoClient } from 'mongodb';
 import dotenv from'dotenv';
 import {moviesRouter} from './routes/movies.js';
 import {userRouter} from './routes/users.js';
+import cors from 'cors';
 
 //const express = require('express') //3rd pary imort
 dotenv.config();
@@ -10,9 +11,11 @@ console.log(process.env.MONGO_URL);
 const app = express() //call express method
 
 
+app.use(cors());
+
 const port =process.env.PORT;
 
-  app.use(express.json());
+  app.use(express.json()); //-->for parsing req and res  header and body object into json object
   const MONGO_URL = process.env.MONGO_URL;
  
   async function createConnection() {
@@ -29,6 +32,20 @@ app.get('/', function (req, res) {
 })
 app.use('/movies', moviesRouter)
 app.use('/users', userRouter)
+app.get('/mobiles', async function (req, res) {
+  const getMobiles = await client
+        .db("B33WD")
+        .collection("mobiles")
+        .find({}).toArray();
+  res.send(getMobiles)
+})
+app.post('/mobiles', async function (req, res) {
+  const data = req.body;
+  const result = await client.db("B33WD")
+  .collection("mobiles")
+  .insertMany(data)
+ res.send(result)
+})
 
 app.listen(port,()=>console.log(`app Started ${port}`))
 
